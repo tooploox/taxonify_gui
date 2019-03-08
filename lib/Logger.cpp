ffaa8ed0d77d1791e5b60845b4b3a44afe4a33aa
Adam Chudas <stafficzek@gmail.com>
2019-03-08 09:49:55 +0100

    AQ-154 better formatting for logs

--- lib/Logger.cpp
+++ lib/Logger.cpp
@@ -1,21 +1,23 @@
 #include "Logger.h"
 
+#include <QDateTime>
 #include <QString>
 #include <QtGlobal>
 
 #include "spdlog/sinks/rotating_file_sink.h"
 
 static constexpr auto logFilename = "logfile.txt";
+static constexpr unsigned max_file_size = 1048576 * 5; // 5 MB
+static constexpr unsigned max_files = 1;
 
-static const QString messagePattern("[%{time yyyyMMdd h:mm:ss.zzz t} %{if-debug}D%{endif}"
-                       "%{if-info}I%{endif}%{if-warning}W%{endif}%{if-critical}C%{endif}"
-                       "%{if-fatal}F%{endif}] %{file}:%{line} - %{message}");
+static const QString messagePattern("%{file}:%{line}\t{%{function}}\t%{message}");
 
 static void messageHandler(QtMsgType type,
                     const QMessageLogContext &context,
                     const QString &message) {
-    static auto logger = spdlog::rotating_logger_mt("logger", logFilename, 1048576 * 5, 1);
-    logger->info(qPrintable(qFormatLogMessage(type, context, message)));
+    static auto logger = spdlog::rotating_logger_mt("logger", logFilename, max_file_size, max_files);
+    QString timeUTC = QDateTime::currentDateTime().toUTC().toString("[yyyy-MM-dd h:mm:ss t] ");
+    logger->info(qPrintable(timeUTC + qFormatLogMessage(type, context, message)));
 }
 
 void initLogging() {
